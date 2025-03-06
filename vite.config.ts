bc39beb9e14f725926f5504cb46ed94d64e679b0
Simon Phillips <simonxphillips@duck.com>
2025-03-06 15:12:09 -0500

    Updated Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/saux-component-library/', // Set the base path for GitHub Pages
   plugins: [react()],
   server: {
     open: true,
