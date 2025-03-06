2cd28cbdd1df9e7d3d3daf18f6d3b99bd1391e8b
Simon Phillips <simonxphillips@duck.com>
2025-03-06 11:38:47 -0500

    Storybook build

---
+++ storybook-static/assets/Tooltip.stories-DEdVHXEV.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{T as n}from"./Tooltip-BPSTs6Tu.js";import"./index-BZISi7jw.js";import"./_commonjsHelpers-CqkleIqs.js";const d={title:"Components/Tooltip",component:n},a=i=>t.jsx("div",{style:{margin:"100px"},children:t.jsx(n,{...i,children:t.jsx("button",{children:"Hover over me"})})}),o=a.bind({});o.args={content:"This is a tooltip!"};var r,e,s;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`args => <div style={{
+  margin: "100px"
+}}>\r
+    <Tooltip {...args}>\r
+      <button>Hover over me</button>\r
+    </Tooltip>\r
+  </div>`,...(s=(e=o.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,d as default};
