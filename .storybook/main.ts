296770d985d2cd09018d701facc7a11a25d6ffb5
Simon Phillips <simonxphillips@duck.com>
2025-03-06 11:42:51 -0500

    Update file naming

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,25 @@
-// filepath: /c:/Users/simonp/Documents/Extra/saux-component-library/.storybook/main.ts
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    
-    "@storybook/addon-a11y",
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-storysource',
     '@storybook/addon-themes',
-    "storybook-dark-mode"
+    'storybook-dark-mode',
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  typescript: {
+    check: true,
+  },
 };
+
 export default config;
